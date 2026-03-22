14593c34eea0bc0a8af3014f299db984edcdbbb4
Rohail Taimour <rohail.taimour@gmail.com>
2026-03-22 21:16:15 +0100

    Add some more types

--- src/working.py
+++ src/working.py
@@ -1,2 +1,13 @@
+def add(a: int, b: int):
+    result = a + b
+    return result
+
 def foo():
     print("hello")
+
+def greet(name: str, age: int):
+    pass
+
+greet("Alice", 30)   
+
+datetime.datetime.now()
